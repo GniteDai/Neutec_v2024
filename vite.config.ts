c71a6b59ca310888d7b93b86e67ebc86123652a9
Dai <gnitekk@gmail.com>
2024-04-26 20:15:54 +0800

    [Fix] vite config : base & gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,8 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
+  base: '/Neutec_v2024/',
+  plugins: [vue(), vueJsx(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
